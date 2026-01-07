8c55034e71a297cf761586f366a9aef797a9195b
aiaccfin <aiaccfin@gmail.com>
2026-01-07 09:13:33 -0500

    pytest

---
+++ fastapi/tests/test_health.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import create_app
+
+client = TestClient(create_app())
+
+def test_app_root():
+    response = client.get("/")
+    assert response.status_code == 200
