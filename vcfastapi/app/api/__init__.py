c969928391073f1641741f17571dbba5c459daa2
aiaccfin <aiaccfin@gmail.com>
2026-01-13 14:58:25 -0500

    update

--- vcfastapi/app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends
-
-from app.api.default import rouDefault
-
-
-rou = APIRouter()
-
-rou.include_router(rouDefault, prefix="", tags=["default"])
--- vcfastapi/app/db/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .m_base import Base, BaseMixin
-from .m_innov import Innov
